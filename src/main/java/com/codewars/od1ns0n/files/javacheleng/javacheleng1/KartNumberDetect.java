391ae1bbbebe171d426760b85d1568a52c39cd88
0d1ns0n <denisow88@gmail.com>
2022-10-16 21:13:37 +0300

    file

---
+++ src/main/java/com/codewars/od1ns0n/files/javacheleng/javacheleng1/KartNumberDetect.java
@@ -0,0 +1,15 @@
+package com.codewars.od1ns0n.files.javacheleng.javacheleng1;
+
+public class KartNumberDetect {
+    public static void main(String[] args) {
+
+    }
+    public static String maskify(String str) {
+        try {
+            throw new UnsupportedOperationException("Unimplemented");
+        }
+        catch (UnsupportedOperationException e){
+            return "try";
+        }
+    }
+}
---
+++ src/main/java/com/codewars/od1ns0n/files/test.java
@@ -0,0 +1,4 @@
+package com.codewars.od1ns0n.files;
+
+public class test {
+}
